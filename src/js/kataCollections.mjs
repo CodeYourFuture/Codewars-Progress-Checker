605ef17e069e6b733087dd86d3d0b7d34bb53788
cjyuan <cjyuan00.gmail.com>
2025-10-02 17:35:45 +0100

    Add 'Array and object methods' collection

--- src/js/kataCollections.mjs
+++ src/js/kataCollections.mjs
@@ -218,6 +218,106 @@ const module_data_flow = [
   }
 ];
 
+const array_and_object_methods = [
+  {
+    "id": "582746fa14b3892727000c4f",
+    "name": "Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe"
+  },
+  {
+    "id": "58279e13c983ca4a2a00002a",
+    "name": "Coding Meetup #2 - Higher-Order Functions Series - Greet developers"
+  },
+  {
+    "id": "5827acd5f524dd029d0005a4",
+    "name": "Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?"
+  },
+  {
+    "id": "5827bc50f524dd029d0005f2",
+    "name": "Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer"
+  },
+  {
+    "id": "58287977ef8d4451f90001a0",
+    "name": "Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?"
+  },
+  {
+    "id": "582ba36cc1901399a70005fc",
+    "name": "Coding Meetup #11 - Higher-Order Functions Series - Find the average age"
+  },
+  {
+    "id": "582dace555a1f4d859000058",
+    "name": "Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins"
+  },
+  {
+    "id": "583952fbc23341c7180002fd",
+    "name": "Coding Meetup #14 - Higher-Order Functions Series - Order the food"
+  },
+  {
+    "id": "580a4734d6df748060000045",
+    "name": "Sorted? yes? no? how?"
+  },
+  {
+    "id": "57ee99a16c8df7b02d00045f",
+    "name": "Flatten and sort an array"
+  },
+  {
+    "id": "54bf85e3d5b56c7a05000cf9",
+    "name": "Testing 1-2-3"
+  },
+  {
+    "id": "582a53ed261c2af9d200018c",
+    "name": "Coding Meetup #10 - Higher-Order Functions Series - Create usernames"
+  },
+  {
+    "id": "5a2be17aee1aaefe2a000151",
+    "name": "Array plus array"
+  },
+  {
+    "id": "583a8bde28019d615a000035",
+    "name": "Coding Meetup #15 - Higher-Order Functions Series - Find the odd names"
+  },
+  {
+    "id": "58381907f8ac48ae070000de",
+    "name": "Coding Meetup #13 - Higher-Order Functions Series - Is the meetup language-diverse?"
+  },
+  {
+    "id": "52597aa56021e91c93000cb0",
+    "name": "Moving Zeros To The End"
+  },
+  {
+    "id": "56b1eb19247c01493a000065",
+    "name": "Unique Sum"
+  },
+  {
+    "id": "523f5d21c841566fde000009",
+    "name": "Array.diff"
+  },
+  {
+    "id": "5905871c00881d0e85000015",
+    "name": "Mix Fruit Juice "
+  },
+  {
+    "id": "54f9173aa58bce9031001548",
+    "name": "The takeWhile Function"
+  },
+  {
+    "id": "514a677421607afc99000002",
+    "name": "getNames()"
+  },
+  {
+    "id": "55d2603d506a40e162000056",
+    "name": "Reducing Problems - Bug Fixing #8"
+  },
+  {
+    "id": "5c8bfa44b9d1192e1ebd3d15",
+    "name": "A wolf in sheep's clothing"
+  },
+  {
+    "id": "5b6c220fa0a661fbf200005d",
+    "name": "Convert the score"
+  }
+]
+
+
 export const kataCollections = [
   {
     name: "ITP: Module Onboarding",
@@ -235,8 +335,13 @@ export const kataCollections = [
     katas: module_data_groups
   },
   {
+    name: "Array and Object Methods",
+    acceptedLanguages: ["JavaScript"],
+    katas: array_and_object_methods
+  },
+  {
     name: "ITP: Module Data Flow",
     acceptedLanguages: ["JavaScript"],
     katas: module_data_flow
-  }  
+  }
 ];
